1339c7a28e4711d1be3524a59600c344484a052e
MaxPrain12 <alder.velasquezobando@gmail.com>
2022-10-25 13:51:40 +0200

    añadiendo el build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-// https://astro.build/config
-export default defineConfig({renderers: ['@astrojs/renderer-react']});
+export default {
+  // ...
+  integrations: [react()],
+}
 
